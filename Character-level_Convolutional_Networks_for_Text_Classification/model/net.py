ce4bb7e37d50c849c3b9e3fbf6a40984af1c05ee
BOSEOP KIM <bsk0130@gmail.com>
2019-04-04 09:50:18 +0900

    Character-level Convolutional Networks for Text Classification

--- Character-level_Convolutional_Networks_for_Text_Classification/model/net.py
+++ Character-level_Convolutional_Networks_for_Text_Classification/model/net.py
@@ -1,6 +1,18 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+
+class Flatten(nn.Module):
+    """Flatten class"""
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.view(x.size(0), -1)
+
+
+class Permute(nn.Module):
+    """Permute class"""
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.permute(0, 2, 1)
+
 
 class CharCNN(nn.Module):
     """CharCNN class"""
@@ -13,33 +25,38 @@ class CharCNN(nn.Module):
             dic (dict): token2idx
         """
         super(CharCNN, self).__init__()
-        self._embedding = nn.Embedding(len(dic), embedding_dim, padding_idx=0)
-        self._conv1 = nn.Conv1d(in_channels=embedding_dim, out_channels=256, kernel_size=7)
-        self._conv2 = nn.Conv1d(in_channels=256, out_channels=256, kernel_size=7)
-        self._conv3 = nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3)
-        self._conv4 = nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3)
-        self._conv5 = nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3)
-        self._conv6 = nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3)
+        self._extractor = nn.Sequential(nn.Embedding(len(dic), embedding_dim, padding_idx=0),
+                                        Permute(),
+                                        nn.Conv1d(in_channels=embedding_dim, out_channels=256, kernel_size=7),
+                                        nn.ReLU(),
+                                        nn.MaxPool1d(3, 3),
+                                        nn.Conv1d(in_channels=256, out_channels=256, kernel_size=7),
+                                        nn.ReLU(),
+                                        nn.MaxPool1d(3, 3),
+                                        nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3),
+                                        nn.ReLU(),
+                                        nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3),
+                                        nn.ReLU(),
+                                        nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3),
+                                        nn.ReLU(),
+                                        nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3),
+                                        nn.ReLU(),
+                                        nn.MaxPool1d(3, 3),
+                                        Flatten())
 
-        self._fc1 = nn.Linear(in_features=1792, out_features=512)
-        self._fc2 = nn.Linear(in_features=512, out_features=512)
-        self._fc3 = nn.Linear(in_features=512, out_features=num_classes)
+        self._classifier = nn.Sequential(nn.Linear(in_features=1792, out_features=512),
+                                         nn.ReLU(),
+                                         nn.Dropout(),
+                                         nn.Linear(in_features=512, out_features=512),
+                                         nn.ReLU(),
+                                         nn.Dropout(),
+                                         nn.Linear(in_features=512, out_features=num_classes))
 
         self.apply(self._init_weights)
 
     def forward(self, x: torch.Tensor, p=.5) -> torch.Tensor:
-        x_batch = self._embedding(x)
-        x_batch = x_batch.permute(0, 2, 1)
-        fmap = F.max_pool1d(F.relu(self._conv1(x_batch)), 3, 3)
-        fmap = F.max_pool1d(F.relu(self._conv2(fmap)), 3, 3)
-        fmap = F.relu(self._conv3(fmap))
-        fmap = F.relu(self._conv4(fmap))
-        fmap = F.relu(self._conv5(fmap))
-        fmap = F.max_pool1d(F.relu(self._conv6(fmap)), 3, 3)
-        flatten = fmap.view(fmap.shape[0], -1)
-        dense = F.dropout(F.relu(self._fc1(flatten)), p=p, training=self.training)
-        dense = F.dropout(F.relu(self._fc2(dense)), p=p, training=self.training)
-        score = self._fc3(dense)
+        feature = self._extractor(x)
+        score = self._classifier(feature)
         return score
 
     def _init_weights(self, layer) -> None:
