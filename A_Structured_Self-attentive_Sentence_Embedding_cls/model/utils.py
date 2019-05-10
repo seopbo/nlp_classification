ced830a0f8ebd0ba8209cc3b909c8db01ab7168c
aisolab <bsk0130@gmail.com>
2019-05-10 09:43:54 +0900

    A Structured Self-attentive Sentence Embedding for classification

--- A_Structured_Self-attentive_Sentence_Embedding_cls/model/utils.py
+++ A_Structured_Self-attentive_Sentence_Embedding_cls/model/utils.py
@@ -15,7 +15,7 @@ def collate_fn(data: List[Tuple[torch.tensor, torch.tensor, torch.tensor]]) ->\
     """
     data = sorted(data, key=lambda elm: elm[2], reverse=True)
     indices, labels, lengths = zip(*data)
-    indices = pad_sequence(indices, batch_first=True)
+    indices = pad_sequence(indices, batch_first=True, padding_value=1)
     labels = torch.stack(labels, 0)
     lengths = torch.stack(lengths, 0)
     return indices, labels, lengths
