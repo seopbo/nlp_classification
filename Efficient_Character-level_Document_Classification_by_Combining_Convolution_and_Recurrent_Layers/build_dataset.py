0ca2fbea45b46ce0938148336204251f831c9c9e
aisolab <bsk0130@gmail.com>
2019-10-27 16:25:28 +0900

    Efficient Character-level Document Classification by Combining Convolution and Recurrent Layers

--- Efficient_Character-level_Document_Classification_by_Combining_Convolution_and_Recurrent_Layers/build_dataset.py
+++ Efficient_Character-level_Document_Classification_by_Combining_Convolution_and_Recurrent_Layers/build_dataset.py
@@ -1,18 +1,28 @@
 import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+from utils import Config
 
 # loading dataset
-cwd = Path.cwd()
-filepath = cwd / 'data/ratings_train.txt'
-dataset = pd.read_csv(filepath, sep='\t').loc[:, ['document', 'label']]
-dataset = dataset.loc[dataset['document'].isna().apply(lambda elm: not elm), :]
+data_dir = Path("data")
+filepath = data_dir / "ratings_train.txt"
+dataset = pd.read_csv(filepath, sep="\t").loc[:, ["document", "label"]]
+dataset = dataset.loc[dataset["document"].isna().apply(lambda elm: not elm), :]
 tr, val = train_test_split(dataset, test_size=0.2, random_state=777)
 
-tr.to_csv(cwd / 'data' / 'train.txt', sep='\t', index=False)
-val.to_csv(cwd / 'data' / 'validation.txt', sep='\t', index=False)
+tr.to_csv(data_dir / "train.txt", sep="\t", index=False)
+val.to_csv(data_dir / "validation.txt", sep="\t", index=False)
 
-tst_filepath = cwd / 'data/ratings_test.txt'
-tst = pd.read_csv(tst_filepath, sep='\t').loc[:, ['document', 'label']]
-tst = tst.loc[tst['document'].isna().apply(lambda elm: not elm), :]
-tst.to_csv(cwd / 'data' / 'test.txt', sep='\t', index=False)
+tst_filepath = data_dir / "ratings_test.txt"
+tst = pd.read_csv(tst_filepath, sep="\t").loc[:, ["document", "label"]]
+tst = tst.loc[tst["document"].isna().apply(lambda elm: not elm), :]
+tst.to_csv(data_dir / "test.txt", sep="\t", index=False)
+
+config = Config(
+    {
+        "train": str(data_dir / "train.txt"),
+        "validation": str(data_dir / "validation.txt"),
+        "test": str(data_dir / "test.txt"),
+    }
+)
+config.save(data_dir / "config.json")
--- Efficient_Character-level_Document_Classification_by_Combining_Convolution_and_Recurrent_Layers/build_vocab.py
+++ Efficient_Character-level_Document_Classification_by_Combining_Convolution_and_Recurrent_Layers/build_vocab.py
@@ -1,5 +1,7 @@
 import pickle
+from pathlib import Path
 from model.utils import Vocab
+from utils import Config
 
 chosung_list = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ',
                 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ',
@@ -12,7 +14,12 @@ jongsung_list = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ',
                  'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
 list_of_jamos = sorted(set(chosung_list + jungsung_list + jongsung_list))
-vocab = Vocab(list_of_jamos, bos_token=None, eos_token=None)
+vocab = Vocab(list_of_tokens=list_of_jamos, bos_token=None, eos_token=None)
 
-with open('data/vocab.pkl', mode='wb') as io:
+data_dir = Path('data')
+with open(data_dir / 'vocab.pkl', mode='wb') as io:
     pickle.dump(vocab, io)
+
+config = Config(data_dir / 'config.json')
+config.update({'vocab': str(data_dir / 'vocab.pkl')})
+config.save(data_dir / 'config.json')
