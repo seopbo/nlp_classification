809b039392eb41f631b8bcfc30f11721672b9353
aisolab <bsk0130@gmail.com>
2019-10-27 17:04:36 +0900

    Very Deep Convolutional Networks for Text Classification

--- Very_Deep_Convolutional_Networks_for_Text_Classification/build_vocab.py
+++ Very_Deep_Convolutional_Networks_for_Text_Classification/build_vocab.py
@@ -1,18 +1,90 @@
 import pickle
+from pathlib import Path
 from model.utils import Vocab
+from utils import Config
 
-chosung_list = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ',
-                'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ',
-                'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
-jungsung_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ',
-                 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ',
-                 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
-jongsung_list = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ',
-                 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ',
-                 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+chosung_list = [
+    "ㄱ",
+    "ㄲ",
+    "ㄴ",
+    "ㄷ",
+    "ㄸ",
+    "ㄹ",
+    "ㅁ",
+    "ㅂ",
+    "ㅃ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅉ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+]
+jungsung_list = [
+    "ㅏ",
+    "ㅐ",
+    "ㅑ",
+    "ㅒ",
+    "ㅓ",
+    "ㅔ",
+    "ㅕ",
+    "ㅖ",
+    "ㅗ",
+    "ㅘ",
+    "ㅙ",
+    "ㅚ",
+    "ㅛ",
+    "ㅜ",
+    "ㅝ",
+    "ㅞ",
+    "ㅟ",
+    "ㅠ",
+    "ㅡ",
+    "ㅢ",
+    "ㅣ",
+]
+jongsung_list = [
+    " ",
+    "ㄱ",
+    "ㄲ",
+    "ㄳ",
+    "ㄴ",
+    "ㄵ",
+    "ㄶ",
+    "ㄷ",
+    "ㄹ",
+    "ㄺ",
+    "ㄻ",
+    "ㄼ",
+    "ㄽ",
+    "ㄾ",
+    "ㄿ",
+    "ㅀ",
+    "ㅁ",
+    "ㅂ",
+    "ㅄ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+]
 
 list_of_jamos = sorted(set(chosung_list + jungsung_list + jongsung_list))
-vocab = Vocab(list_of_jamos, bos_token=None, eos_token=None)
+vocab = Vocab(list_of_tokens=list_of_jamos, bos_token=None, eos_token=None)
 
-with open('data/vocab.pkl', mode='wb') as io:
+data_dir = Path("data")
+with open(data_dir / "vocab.pkl", mode="wb") as io:
     pickle.dump(vocab, io)
+
+config = Config(data_dir / "config.json")
+config.update({"vocab": str(data_dir / "vocab.pkl")})
+config.save(data_dir / "config.json")
--- Very_Deep_Convolutional_Networks_for_Text_Classification/evaluate.py
+++ Very_Deep_Convolutional_Networks_for_Text_Classification/evaluate.py
@@ -14,17 +14,15 @@ from utils import Config, CheckpointManager, SummaryManager
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='data', help="Directory containing config.json of data")
 parser.add_argument('--model_dir', default='experiments/base_model', help="Directory containing config.json of model")
-parser.add_argument('--restore_file', default='best', help="name of the file in --model_dir \
-                     containing weights to load")
-parser.add_argument('--data_name', default='test', help="name of the data in --data_dir to be evaluate")
+parser.add_argument('--dataset', default='test', help="name of the data in --data_dir to be evaluate")
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     data_dir = Path(args.data_dir)
     model_dir = Path(args.model_dir)
-    data_config = Config(json_path=data_dir / 'config.json')
-    model_config = Config(json_path=model_dir / 'config.json')
+    data_config = Config(data_dir / 'config.json')
+    model_config = Config(model_dir / 'config.json')
 
     # tokenizer
     with open(data_config.vocab, mode='rb') as io:
@@ -34,14 +32,14 @@ if __name__ == '__main__':
 
     # model (restore)
     checkpoint_manager = CheckpointManager(model_dir)
-    checkpoint = checkpoint_manager.load_checkpoint(args.restore_file + '.tar')
+    checkpoint = checkpoint_manager.load_checkpoint('best.tar')
     model = VDCNN(num_classes=model_config.num_classes, embedding_dim=model_config.embedding_dim,
                   k_max=model_config.k_max, vocab=tokenizer.vocab)
     model.load_state_dict(checkpoint['model_state_dict'])
 
     # evaluation
     summary_manager = SummaryManager(model_dir)
-    filepath = getattr(data_config, args.data_name)
+    filepath = getattr(data_config, args.dataset)
     ds = Corpus(filepath, tokenizer.split_and_transform)
     dl = DataLoader(ds, batch_size=model_config.batch_size, num_workers=4)
 
@@ -51,7 +49,7 @@ if __name__ == '__main__':
     summary = evaluate(model, dl, {'loss': nn.CrossEntropyLoss(), 'acc': acc}, device)
 
     summary_manager.load('summary.json')
-    summary_manager.update({'{}'.format(args.data_name): summary})
+    summary_manager.update({'{}'.format(args.dataset): summary})
     summary_manager.save('summary.json')
 
     print('loss: {:.3f}, acc: {:.2%}'.format(summary['loss'], summary['acc']))
