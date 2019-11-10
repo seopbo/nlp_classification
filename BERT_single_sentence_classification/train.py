525a907250bf8237c0a3b698504720c8faa7360e
aisolab <bsk0130@gmail.com>
2019-11-10 15:25:52 +0900

    BERT single sentence classification

--- BERT_single_sentence_classification/train.py
+++ BERT_single_sentence_classification/train.py
@@ -6,7 +6,8 @@ import torch.optim as optim
 from pathlib import Path
 from torch.utils.data import DataLoader
 from transformers.modeling_bert import BertConfig
-from pretrained.tokenization import BertTokenizer
+from pretrained.tokenization import BertTokenizer as ETRITokenizer
+from gluonnlp.data import SentencepieceTokenizer
 from model.net import SentenceClassifier
 from model.data import Corpus
 from model.utils import PreProcessor, PadSequence
@@ -23,26 +24,37 @@ torch.backends.cudnn.benchmark = False
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='data', help="Directory containing config.json of data")
 parser.add_argument('--model_dir', default='experiments/base_model', help="Directory containing config.json of model")
+parser.add_argument('--type', default='skt', choices=['skt', 'etri'], required=True,  type=str)
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    ptr_dir = Path('pretrained')
     data_dir = Path(args.data_dir)
     model_dir = Path(args.model_dir)
+
+    ptr_config = Config(ptr_dir / 'config_{}.json'.format(args.type))
     data_config = Config(data_dir / 'config.json')
     model_config = Config(model_dir / 'config.json')
 
-    # tokenizer
-    ptr_tokenizer = BertTokenizer.from_pretrained('pretrained/vocab.korean.rawtext.list', do_lower_case=False)
-    with open(data_config.vocab, mode='rb') as io:
+    # vocab
+    with open(ptr_config.vocab, mode='rb') as io:
         vocab = pickle.load(io)
-    pad_sequence = PadSequence(length=model_config.length, pad_val=vocab.to_indices(vocab.padding_token))
-    preprocessor = PreProcessor(vocab=vocab, split_fn=ptr_tokenizer.tokenize, pad_fn=pad_sequence)
+
+    # tokenizer
+    if args.type == 'etri':
+        ptr_tokenizer = ETRITokenizer.from_pretrained(ptr_config.tokenizer, do_lower_case=False)
+        pad_sequence = PadSequence(length=model_config.length, pad_val=vocab.to_indices(vocab.padding_token))
+        preprocessor = PreProcessor(vocab=vocab, split_fn=ptr_tokenizer.tokenize, pad_fn=pad_sequence)
+    elif args.type == 'skt':
+        ptr_tokenizer = SentencepieceTokenizer(ptr_config.tokenizer)
+        pad_sequence = PadSequence(length=model_config.length, pad_val=vocab.to_indices(vocab.padding_token))
+        preprocessor = PreProcessor(vocab=vocab, split_fn=ptr_tokenizer, pad_fn=pad_sequence)
 
     # model
-    config = BertConfig('pretrained/bert_config.json')
+    config = BertConfig(ptr_config.config)
     model = SentenceClassifier(config, num_classes=model_config.num_classes, vocab=preprocessor.vocab)
-    bert_pretrained = torch.load('pretrained/pytorch_model.bin')
+    bert_pretrained = torch.load(ptr_config.bert)
     model.load_state_dict(bert_pretrained, strict=False)
 
     # training
@@ -59,10 +71,10 @@ if __name__ == '__main__':
 
         ], weight_decay=5e-4)
 
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('cuda:1') if torch.cuda.is_available() else torch.device('cpu')
     model.to(device)
 
-    writer = SummaryWriter('{}/runs'.format(model_dir))
+    writer = SummaryWriter('{}/runs_{}'.format(model_dir, args.type))
     checkpoint_manager = CheckpointManager(model_dir)
     summary_manager = SummaryManager(model_dir)
     best_val_loss = 1e+10
@@ -116,7 +128,7 @@ if __name__ == '__main__':
                 summary = {'train': tr_summary, 'validation': val_summary}
 
                 summary_manager.update(summary)
-                summary_manager.save('summary.json')
-                checkpoint_manager.save_checkpoint(state, 'best.tar')
+                summary_manager.save('summary_{}.json'.format(args.type))
+                checkpoint_manager.save_checkpoint(state, 'best_{}.tar'.format(args.type))
 
                 best_val_loss = val_loss
