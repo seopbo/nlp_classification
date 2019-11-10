1131bf80d9349440adeab507185e074cf2d74108
aisolab <bsk0130@gmail.com>
2019-11-10 15:47:15 +0900

    BERT pairwise text classification

--- BERT_pairwise_text_classification/evaluate.py
+++ BERT_pairwise_text_classification/evaluate.py
@@ -5,7 +5,8 @@ import torch.nn as nn
 from pathlib import Path
 from torch.utils.data import DataLoader
 from transformers.modeling_bert import BertConfig
-from pretrained.tokenization import BertTokenizer
+from pretrained.tokenization import BertTokenizer as ETRITokenizer
+from gluonnlp.data import SentencepieceTokenizer
 from model.net import PairwiseClassifier
 from model.data import Corpus
 from model.utils import PreProcessor, PadSequence
@@ -15,27 +16,38 @@ from utils import Config, CheckpointManager, SummaryManager
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='data', help="Directory containing config.json of data")
 parser.add_argument('--model_dir', default='experiments/base_model', help="Directory containing config.json of model")
-parser.add_argument('--dataset', default='validation', help="name of the data in --data_dir to be evaluate")
+parser.add_argument('--dataset', default='test', help="name of the data in --data_dir to be evaluate")
+parser.add_argument('--type', default='skt', choices=['skt', 'etri'], required=True,  type=str)
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    ptr_dir = Path('pretrained')
     data_dir = Path(args.data_dir)
     model_dir = Path(args.model_dir)
+
+    ptr_config = Config(ptr_dir / 'config_{}.json'.format(args.type))
     data_config = Config(data_dir / 'config.json')
     model_config = Config(model_dir / 'config.json')
 
-    # tokenizer
-    ptr_tokenizer = BertTokenizer.from_pretrained('pretrained/vocab.korean.rawtext.list', do_lower_case=False)
-    with open(data_config.vocab, mode='rb') as io:
+    # vocab
+    with open(ptr_config.vocab, mode='rb') as io:
         vocab = pickle.load(io)
-    pad_sequence = PadSequence(length=model_config.length, pad_val=vocab.to_indices(vocab.padding_token))
-    preprocessor = PreProcessor(vocab=vocab, split_fn=ptr_tokenizer.tokenize, pad_fn=pad_sequence)
+
+    # tokenizer
+    if args.type == 'etri':
+        ptr_tokenizer = ETRITokenizer.from_pretrained(ptr_config.tokenizer, do_lower_case=False)
+        pad_sequence = PadSequence(length=model_config.length, pad_val=vocab.to_indices(vocab.padding_token))
+        preprocessor = PreProcessor(vocab=vocab, split_fn=ptr_tokenizer.tokenize, pad_fn=pad_sequence)
+    elif args.type == 'skt':
+        ptr_tokenizer = SentencepieceTokenizer(ptr_config.tokenizer)
+        pad_sequence = PadSequence(length=model_config.length, pad_val=vocab.to_indices(vocab.padding_token))
+        preprocessor = PreProcessor(vocab=vocab, split_fn=ptr_tokenizer, pad_fn=pad_sequence)
 
     # model (restore)
     checkpoint_manager = CheckpointManager(model_dir)
-    checkpoint = checkpoint_manager.load_checkpoint('best.tar')
-    config = BertConfig('pretrained/bert_config.json')
+    checkpoint = checkpoint_manager.load_checkpoint('best_{}.tar'.format(args.type))
+    config = BertConfig(ptr_config.config)
     model = PairwiseClassifier(config, num_classes=model_config.num_classes, vocab=preprocessor.vocab)
     model.load_state_dict(checkpoint['model_state_dict'])
 
@@ -50,8 +62,8 @@ if __name__ == '__main__':
     summary_manager = SummaryManager(model_dir)
     summary = evaluate(model, dl, {'loss': nn.CrossEntropyLoss(), 'acc': acc}, device)
 
-    summary_manager.load('summary.json')
+    summary_manager.load('summary_{}.json'.format(args.type))
     summary_manager.update({'{}'.format(args.dataset): summary})
-    summary_manager.save('summary.json')
+    summary_manager.save('summary_{}.json'.format(args.type))
 
     print('loss: {:.3f}, acc: {:.2%}'.format(summary['loss'], summary['acc']))
---
+++ BERT_pairwise_text_classification/prepare_vocab_and_weights.py
@@ -0,0 +1,98 @@
+import argparse
+import torch
+import pickle
+from collections import OrderedDict
+from pathlib import Path
+from model.utils import Vocab
+from utils import Config
+from pretrained.tokenization import BertTokenizer as ETRITokenizer
+from gluonnlp.vocab import BERTVocab
+from urllib.request import urlretrieve
+
+parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="prepare pretrained-bert")
+parser.add_argument("--type", type=str, choices=["skt", "etri"], default="skt", required=True)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    ptr_dir = Path('pretrained')
+
+    if args.type == 'skt':
+        ptr_config_path = ptr_dir / 'bert_config_skt.json'
+        ptr_bert_path = ptr_dir / 'pytorch_model_skt.bin'
+        ptr_vocab_path = ptr_dir / 'pytorch_model_skt_vocab.json'
+        ptr_tokenizer_path = ptr_dir /'pytorch_model_skt_tokenizer.model'
+
+        if not ptr_bert_path.exists():
+            urlretrieve('https://kobert.blob.core.windows.net/models/kobert/pytorch/pytorch_kobert_2439f391a6.params',
+                        filename=ptr_bert_path)
+            ptr_bert = torch.load(ptr_bert_path)
+            ptr_bert = OrderedDict([(('bert.' + k), ptr_bert.get(k)) for k in ptr_bert.keys()])
+            torch.save(ptr_bert, ptr_bert_path)
+        else:
+            print('Already you have pytorch_model_skt.bin!')
+
+        if not ptr_vocab_path.exists():
+            urlretrieve('https://kobert.blob.core.windows.net/models/kobert/vocab/kobertvocab_f38b8a4d6d.json',
+                        filename=ptr_vocab_path)
+            ptr_bert_vocab = BERTVocab.from_json(ptr_vocab_path.open(mode='rt').read())
+            vocab = Vocab(ptr_bert_vocab.idx_to_token,
+                          padding_token="[PAD]",
+                          unknown_token="[UNK]",
+                          bos_token=None,
+                          eos_token=None,
+                          reserved_tokens=["[CLS]", "[SEP]", "[MASK]"],
+                          token_to_idx=ptr_bert_vocab.token_to_idx)
+
+            # save vocab
+            with open(ptr_vocab_path.with_suffix('.pkl'), mode="wb") as io:
+                pickle.dump(vocab, io)
+        else:
+            print('Already you have pytorch_model_skt_vocab.json!')
+
+        if not ptr_tokenizer_path.exists():
+            urlretrieve('https://kobert.blob.core.windows.net/models/kobert/tokenizer/tokenizer_78b3253a26.model',
+                        filename=ptr_tokenizer_path)
+        else:
+            print('Already you have pytorch_model_skt_tokenizer.model')
+
+        ptr_config = Config({'config': str(ptr_config_path),
+                             'bert': str(ptr_bert_path),
+                             'tokenizer': str(ptr_tokenizer_path),
+                             'vocab': str(ptr_vocab_path.with_suffix('.pkl'))})
+        ptr_config.save(ptr_dir / "config_skt.json")
+
+    if args.type == 'etri':
+        # loading BertTokenizer
+        ptr_config_path = ptr_dir / 'bert_config_etri.json'
+        ptr_tokenizer_path = ptr_dir / "vocab.korean.rawtext.list"
+        ptr_bert_path = ptr_dir / "pytorch_model_etri.bin"
+
+        ptr_tokenizer = ETRITokenizer.from_pretrained(
+            ptr_tokenizer_path, do_lower_case=False
+        )
+        # generate vocab
+        idx_to_token = list(ptr_tokenizer.vocab.keys())
+        token_to_idx = {token: idx for idx, token in enumerate(idx_to_token)}
+
+        vocab = Vocab(
+            idx_to_token,
+            padding_token="[PAD]",
+            unknown_token="[UNK]",
+            bos_token=None,
+            eos_token=None,
+            reserved_tokens=["[CLS]", "[SEP]", "[MASK]"],
+            token_to_idx=token_to_idx,
+        )
+
+        # save vocab
+        ptr_vocab_path = ptr_dir / "vocab_etri.pkl"
+        with open(ptr_vocab_path, mode="wb") as io:
+            pickle.dump(vocab, io)
+
+        ptr_config = Config({'config': str(ptr_config_path),
+                             'bert': str(ptr_bert_path),
+                             'tokenizer': str(ptr_tokenizer_path),
+                             'vocab': str(ptr_vocab_path)})
+        ptr_config.save(ptr_dir / "config_etri.json")
