4e91688cc2e9a799e9edffb8fb73f7d4b171fc5b
aisolab <bsk0130@gmail.com>
2020-03-04 23:06:26 +0900

    refactoring the project structure of CharCNN implementation

--- Character-level_Convolutional_Networks_for_Text_Classification/evaluate.py
+++ Character-level_Convolutional_Networks_for_Text_Classification/evaluate.py
@@ -11,45 +11,67 @@ from model.utils import Tokenizer, PadSequence
 from model.metric import evaluate, acc
 from utils import Config, CheckpointManager, SummaryManager
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', default='data', help="Directory containing config.json of data")
-parser.add_argument('--model_dir', default='experiments/base_model', help="Directory containing config.json of model")
-parser.add_argument('--dataset', default='test', help="name of the data in --data_dir to be evaluate")
 
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    data_dir = Path(args.data_dir)
-    model_dir = Path(args.model_dir)
-    data_config = Config(data_dir / 'config.json')
-    model_config = Config(model_dir / 'config.json')
-
-    # tokenizer
-    with open(data_config.vocab, mode='rb') as io:
+def get_tokenizer(dataset_config, model_config):
+    with open(dataset_config.vocab, mode='rb') as io:
         vocab = pickle.load(io)
     pad_sequence = PadSequence(length=model_config.length, pad_val=vocab.to_indices(vocab.padding_token))
     tokenizer = Tokenizer(vocab=vocab, split_fn=split_to_jamo, pad_fn=pad_sequence)
+    return tokenizer
 
-    # model (restore)
-    checkpoint_manager = CheckpointManager(model_dir)
-    checkpoint = checkpoint_manager.load_checkpoint('best.tar')
+
+def main(args):
+    dataset_config = Config(args.dataset_config)
+    model_config = Config(args.model_config)
+    exp_dir = Path("experiments") / model_config.type
+    exp_dir = exp_dir.joinpath(
+        f"epochs_{args.epochs}_batch_size_{args.batch_size}_learning_rate_{args.learning_rate}"
+    )
+
+    tokenizer = get_tokenizer(dataset_config, model_config)
+
+    checkpoint_manager = CheckpointManager(exp_dir)
+    checkpoint = checkpoint_manager.load_checkpoint("best.tar")
     model = CharCNN(num_classes=model_config.num_classes, embedding_dim=model_config.embedding_dim,
                     vocab=tokenizer.vocab)
-    model.load_state_dict(checkpoint['model_state_dict'])
+    model.load_state_dict(checkpoint["model_state_dict"])
 
-    # evaluation
-    summary_manager = SummaryManager(model_dir)
-    filepath = getattr(data_config, args.dataset)
+    summary_manager = SummaryManager(exp_dir)
+    filepath = getattr(dataset_config, args.data)
     ds = Corpus(filepath, tokenizer.split_and_transform)
-    dl = DataLoader(ds, batch_size=model_config.batch_size, num_workers=4)
+    dl = DataLoader(ds, batch_size=args.batch_size, num_workers=4)
 
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
 
-    summary = evaluate(model, dl, {'loss': nn.CrossEntropyLoss(), 'acc': acc}, device)
+    summary = evaluate(model, dl, {"loss": nn.CrossEntropyLoss(), "acc": acc}, device)
 
-    summary_manager.load('summary.json')
-    summary_manager.update({'{}'.format(args.dataset): summary})
-    summary_manager.save('summary.json')
+    summary_manager.load("summary.json")
+    summary_manager.update({f"{args.data}": summary})
+    summary_manager.save("summary.json")
+    print(f"loss: {summary['loss']:.3f}, acc: {summary['acc']:.2%}")
 
-    print('loss: {:.3f}, acc: {:.2%}'.format(summary['loss'], summary['acc']))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data", default="test", help="name of the data in nsmc_dir to be evaluate"
+    )
+    parser.add_argument(
+        "--dataset_config",
+        default="conf/dataset/nsmc.json",
+        help="directory containing nsmc.json",
+    )
+    parser.add_argument(
+        "--model_config",
+        default="conf/model/charcnn.json",
+        help="directory containing charcnn.json",
+    )
+    parser.add_argument("--epochs", default=5, help="number of epochs of training")
+    parser.add_argument("--batch_size", default=256, help="batch size of training")
+    parser.add_argument(
+        "--learning_rate", default=1e-3, help="learning rate of training"
+    )
+
+    args = parser.parse_args()
+    main(args)
