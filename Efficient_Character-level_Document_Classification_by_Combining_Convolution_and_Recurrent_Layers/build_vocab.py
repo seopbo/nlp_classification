2980daea19c451b35a0aed82ecc14f19e37d5138
aisolab <bsk0130@gmail.com>
2020-03-23 22:58:49 +0900

    refactoring the project structure of ConvRec implementation

--- Efficient_Character-level_Document_Classification_by_Combining_Convolution_and_Recurrent_Layers/build_vocab.py
+++ Efficient_Character-level_Document_Classification_by_Combining_Convolution_and_Recurrent_Layers/build_vocab.py
@@ -3,23 +3,32 @@ from pathlib import Path
 from model.utils import Vocab
 from utils import Config
 
-chosung_list = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ',
-                'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ',
-                'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
-jungsung_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ',
-                 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ',
-                 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
-jongsung_list = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ',
-                 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ',
-                 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+LIST_OF_CHOSUNG = [
+    "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ",
+    "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ",
+    "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ",
+    "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+LIST_OF_JUNGSUNG = [
+    "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ",
+    "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ",
+    "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ",
+    "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ",
+    "ㅣ"]
+LIST_OF_JONGSUNG = [
+    " ", "ㄱ", "ㄲ", "ㄳ", "ㄴ",
+    "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ",
+    "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ",
+    "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ",
+    "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ",
+    "ㅌ", "ㅍ", "ㅎ"]
 
-list_of_jamos = sorted(set(chosung_list + jungsung_list + jongsung_list))
-vocab = Vocab(list_of_tokens=list_of_jamos, bos_token=None, eos_token=None)
+LIST_OF_JAMOS = sorted(set(LIST_OF_CHOSUNG + LIST_OF_JUNGSUNG + LIST_OF_JONGSUNG))
+vocab = Vocab(list_of_tokens=LIST_OF_JAMOS, bos_token=None, eos_token=None)
+nsmc_dir = Path("nsmc")
 
-data_dir = Path('data')
-with open(data_dir / 'vocab.pkl', mode='wb') as io:
+with open(nsmc_dir / "vocab.pkl", mode="wb") as io:
     pickle.dump(vocab, io)
 
-config = Config(data_dir / 'config.json')
-config.update({'vocab': str(data_dir / 'vocab.pkl')})
-config.save(data_dir / 'config.json')
+config = Config("conf/dataset/nsmc.json")
+config.update({"vocab": str(nsmc_dir / "vocab.pkl")})
+config.save("conf/dataset/nsmc.json")
--- Efficient_Character-level_Document_Classification_by_Combining_Convolution_and_Recurrent_Layers/train.py
+++ Efficient_Character-level_Document_Classification_by_Combining_Convolution_and_Recurrent_Layers/train.py
@@ -15,53 +15,61 @@ from model.utils import Tokenizer
 from model.metric import evaluate, acc
 from utils import Config, CheckpointManager, SummaryManager
 
-# for reproducibility
-torch.manual_seed(777)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', default='data', help="Directory containing config.json of data")
-parser.add_argument('--model_dir', default='experiments/base_model', help="Directory containing config.json of model")
-
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    data_dir = Path(args.data_dir)
-    model_dir = Path(args.model_dir)
-    data_config = Config(data_dir / 'config.json')
-    model_config = Config(model_dir / 'config.json')
-
-    # tokenizer
-    with open(data_config.vocab, mode='rb') as io:
+def get_tokenizer(dataset_config):
+    with open(dataset_config.vocab, mode='rb') as io:
         vocab = pickle.load(io)
     tokenizer = Tokenizer(vocab=vocab, split_fn=split_to_jamo)
+    return tokenizer
+
+
+def get_data_loaders(dataset_config, model_config, tokenizer, batch_size, collate_fn=batchify):
+    tr_ds = Corpus(dataset_config.train, tokenizer.split_and_transform, min_length=model_config.min_length,
+                   pad_val=tokenizer.vocab.to_indices(' '))
+    tr_dl = DataLoader(tr_ds, batch_size=batch_size, shuffle=True, num_workers=4,
+                       collate_fn=collate_fn, drop_last=True)
+    val_ds = Corpus(dataset_config.validation, tokenizer.split_and_transform, min_length=model_config.min_length,
+                   pad_val=tokenizer.vocab.to_indices(' '))
+    val_dl = DataLoader(val_ds, batch_size=batch_size, collate_fn=collate_fn)
+    return tr_dl, val_dl
+
+
+def main(args):
+    dataset_config = Config(args.dataset_config)
+    model_config = Config(args.model_config)
+
+    exp_dir = Path("experiments") / model_config.type
+    exp_dir = exp_dir.joinpath(
+        f"epochs_{args.epochs}_batch_size_{args.batch_size}_learning_rate_{args.learning_rate}"
+    )
+
+    if not exp_dir.exists():
+        exp_dir.mkdir(parents=True)
+
+    if args.fix_seed:
+        torch.manual_seed(777)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    tokenizer = get_tokenizer(dataset_config)
+    tr_dl, val_dl = get_data_loaders(dataset_config, model_config, tokenizer, args.batch_size)
 
     # model
     model = ConvRec(num_classes=model_config.num_classes, embedding_dim=model_config.embedding_dim,
                     hidden_dim=model_config.hidden_dim, vocab=tokenizer.vocab)
 
-    # training
-    tr_ds = Corpus(data_config.train, tokenizer.split_and_transform, min_length=model_config.min_length,
-                   pad_val=tokenizer.vocab.to_indices(' '))
-    tr_dl = DataLoader(tr_ds, batch_size=model_config.batch_size, shuffle=True, num_workers=4,
-                       collate_fn=batchify, drop_last=True)
-    val_ds = Corpus(data_config.validation, tokenizer.split_and_transform, min_length=model_config.min_length,
-                   pad_val=tokenizer.vocab.to_indices(' '))
-    val_dl = DataLoader(val_ds, batch_size=model_config.batch_size, collate_fn=batchify)
-
     loss_fn = nn.CrossEntropyLoss()
-    opt = optim.Adam(params=model.parameters(), lr=model_config.learning_rate)
+    opt = optim.Adam(params=model.parameters(), lr=args.learning_rate)
     scheduler = ReduceLROnPlateau(opt, patience=5)
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model.to(device)
 
-    writer = SummaryWriter('{}/runs'.format(model_dir))
-    checkpoint_manager = CheckpointManager(model_dir)
-    summary_manager = SummaryManager(model_dir)
+    writer = SummaryWriter('{}/runs'.format(exp_dir))
+    checkpoint_manager = CheckpointManager(exp_dir)
+    summary_manager = SummaryManager(exp_dir)
     best_val_loss = 1e+10
 
-    for epoch in tqdm(range(model_config.epochs), desc='epochs'):
+    for epoch in tqdm(range(args.epochs), desc='epochs'):
 
         tr_loss = 0
         tr_acc = 0
@@ -82,7 +90,7 @@ if __name__ == '__main__':
             tr_loss += mb_loss.item()
             tr_acc += mb_acc.item()
 
-            if (epoch * len(tr_dl) + step) % model_config.summary_step == 0:
+            if (epoch * len(tr_dl) + step) % args.summary_step == 0:
                 val_loss = evaluate(model, val_dl, {'loss': loss_fn}, device)['loss']
                 writer.add_scalars('loss', {'train': tr_loss / (step + 1),
                                             'val': val_loss}, epoch * len(tr_dl) + step)
@@ -113,3 +121,29 @@ if __name__ == '__main__':
                 checkpoint_manager.save_checkpoint(state, 'best.tar')
 
                 best_val_loss = val_loss
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset_config",
+        default="conf/dataset/nsmc.json",
+        help="directory containing nsmc.json",
+    )
+    parser.add_argument(
+        "--model_config",
+        default="conf/model/convrec.json",
+        help="directory containing convrec.json",
+    )
+    parser.add_argument("--epochs", default=5, help="number of epochs of training")
+    parser.add_argument("--batch_size", default=256, help="batch size of training")
+    parser.add_argument(
+        "--learning_rate", default=1e-3, help="learning rate of training"
+    )
+    parser.add_argument(
+        "--summary_step", default=500, help="logging performance at each step"
+    )
+    parser.add_argument("--fix_seed", action="store_true", default=False)
+
+    args = parser.parse_args()
+    main(args)
